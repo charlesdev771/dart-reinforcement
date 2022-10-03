a4e73ee3be0fc19d609d7d2ba1d70eed9868f861
charlesdev771 <charlesdev0721@protonmail.com>
2022-10-03 20:30:34 -0300

    FIrst

---
+++ hello.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+
+void funcao1()
+{
+  print('funcao1');
+}
+
+
+void main(List <String> args)
+{
+
+  const number = 1;
+
+  if (number == 2)
+  {
+    funcao1();
+  }
+  else
+  {
+    funcao2();
+  }
+
+  
+}
+
+
+
+void funcao2()
+{
+  print('funcao2');
+}
+
